452620a70b188ae6569a363f3a822717427536ed
Brian K <karabinchak.brian@gmail.com>
2017-05-12 10:38:19 -0400

    Added ID to iFrame of Map, JS now centers on the ZipCode

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,16 +1,23 @@
 var googleMapsAPI = 'AIzaSyAQ34rbfQcs_hp036e8ORnMuoAfULzj74U';
+var longitude, latitude;
 
 //Possible To-Do - make array to check state codes against AL, NJ, TX, etc
-
-function findStateandCity (zipCode) {
-	
+function findStateandCity (zipCode) {	
 	var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+zipCode;
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
 	}).done(function (response) {
+		//extract State and longitude/latitude from Zip Code
 		var state;
-		console.log(response.results[0].address_components[3].short_name);
+		var test = response.results[0].geometry.location;
+		longitude = response.results[0].geometry.location.lng;
+		latitude = response.results[0].geometry.location.lat;
+		console.log(response.results[0].geometry.location);
+		console.log('lng: ' + longitude);
+		console.log('lat: ' + latitude);
+		var baseCenterMap = 'https://www.google.com/maps/embed/v1/view?key='+googleMapsAPI+'&center='+latitude+','+longitude+'&zoom=12';
+		console.log('Center Map URL');
 		//look through elements in components
 		//find first one with 2 letter shortname, return as it is state (hopfully)
 		//NEED TO REFINE THIS
@@ -22,7 +29,7 @@ function findStateandCity (zipCode) {
 				break;
 			}
 		}
-
+		$('#gMap').attr('src', baseCenterMap);
 		console.log('State Found: ' + state);
 		runProPublicaAPI(state);
 	});
