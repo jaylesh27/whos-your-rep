d3dc0ff7d3b24599b84d8609869361284c8dd4d1
unknown <jaylesh27@gmail.com>
2017-05-16 18:15:03 -0400

    fixed zipcode text issue in search box

--- assets/javascript/googleCharts.js
+++ assets/javascript/googleCharts.js
@@ -10,19 +10,19 @@ function drawChart() {
 	data.addColumn('string', 'Topping');
 	data.addColumn('number', 'Slices');
 	data.addRows([
-		['Mushrooms', 3],
-		['Onions', 1],
-		['Olives', 1],
-		['Zucchini', 1],
-		['Pepperoni', 2]
+		['Donor 1', 100],
+		['Donor 2', 200],
+		['Donor 3', 50],
+		['Donor 4', 23],
+		['Donor 5', 500]
 	]);
 
 	// Set chart options
-	var options = {'title':'What is your rep up to?',
+	var options = {'title':'Who is this rep getting money from?',
 					'width':700,
 					'height':500};
 
 	// Instantiate and draw our chart, passing in some options.
-	var chart = new google.visualization.ColumnChart(document.getElementById('infoPanel'));
-		chart.draw(data, options);
+	var chart = new google.visualization.PieChart(document.getElementById('infoPanel'));
+	chart.draw(data, options);
 	}
