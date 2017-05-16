cb740225f3830d0a38ec2e1b07801ea979ce7b73
Brian K <karabinchak.brian@gmail.com>
2017-05-16 19:49:27 -0400

    Able to receive API objects from GET using cross origin me

--- assets/javascript/openSecrets.js
+++ assets/javascript/openSecrets.js
@@ -1,8 +1,24 @@
 var openSecretsAPI = '0e11c0ea5a983dc2004af32bd38156c1';
-var baseQuery = ''
+var norcross = 'N00036154';
 
-$.ajax({
+function getContributionSummary (crID){
+	console.log('running Open Secrets function');
+	var baseQuery = 'https://crossorigin.me/http://www.opensecrets.org/api/?method=candIndustry&output=json&cid=N00007360&cycle=2016&apikey='+openSecretsAPI;
+	console.log(baseQuery);
+	$.ajax({
+		url: baseQuery,
+		method: 'GET',
+		dataType: 'json'
+	}).done(function (data) {
+		console.log('Open Secrets API connected, response:');
+		console.log(data);
 
-}).done(function (response){
+		for (var i = 0; i < data.response.industries.industry.length; i++) {
+			console.log(data.response.industries.industry[i]['@attributes'].industry_name);
+			console.log(data.response.industries.industry[i]['@attributes'].total);
+		}
 
-});
+	});
+}
+
+getContributionSummary(norcross);
