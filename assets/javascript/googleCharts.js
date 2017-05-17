600cbcd5c7a3845ea5b2a95d14e5846b1cd37f9c
Brian K <karabinchak.brian@gmail.com>
2017-05-16 20:35:13 -0400

    Created graphs of open Secrets data using Google charts, rearragned JS in index

--- assets/javascript/googleCharts.js
+++ assets/javascript/googleCharts.js
@@ -5,24 +5,44 @@ google.charts.load('current', {packages: ['corechart']});
 
 function drawChart() {
 
-	// Create the data table.
-	var data = new google.visualization.DataTable();
-	data.addColumn('string', 'Topping');
-	data.addColumn('number', 'Slices');
-	data.addRows([
-		['Donor 1', 100],
-		['Donor 2', 200],
-		['Donor 3', 50],
-		['Donor 4', 23],
-		['Donor 5', 500]
-	]);
+	var openSecretsAPI = '0e11c0ea5a983dc2004af32bd38156c1';
 
-	// Set chart options
-	var options = {'title':'Who is this rep getting money from?',
-					'width':700,
-					'height':500};
+	var baseQuery = 'https://crossorigin.me/http://www.opensecrets.org/api/?method=candIndustry&output=json&cid=N00007360&cycle=2016&apikey='+openSecretsAPI;
+	console.log(baseQuery);
+	$.ajax({
+		url: baseQuery,
+		method: 'GET',
+		dataType: 'json'
+	}).done(function (resp) {
+		console.log('Open Secrets API connected, response:');
+		//create arrays to store info
+		var industrynames=[];
+		var totals=[];
 
-	// Instantiate and draw our chart, passing in some options.
-	var chart = new google.visualization.PieChart(document.getElementById('infoPanel'));
-	chart.draw(data, options);
-	}
+		for (var i = 0; i < resp.response.industries.industry.length; i++) {
+			industrynames.push(resp.response.industries.industry[i]['@attributes'].industry_name);
+			totals.push(resp.response.industries.industry[i]['@attributes'].total);
+		}
+
+		//create Rows for data table
+		var rows = [];
+		for (var j = 0; j < industrynames.length; j++) {
+			rows.push([industrynames[j], parseInt(totals[j])]);
+		}
+
+		// Create the data table.
+      	var data = new google.visualization.DataTable();
+		data.addColumn('string', 'Topping');
+		data.addColumn('number', 'Slices');
+		data.addRows(rows);
+
+		// Set chart options
+		var options = {'title':'Who is this rep getting money from?',
+						'width':700,
+						'height':500};
+
+		// Instantiate and draw our chart, passing in some options.
+		var chart = new google.visualization.PieChart(document.getElementById('infoPanel'));
+		chart.draw(data, options);
+		});
+	};
