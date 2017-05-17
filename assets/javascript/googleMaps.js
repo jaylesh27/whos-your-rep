4798909181ad833db0f9677330d4ac62ea11a80d
Brian K <karabinchak.brian@gmail.com>
2017-05-16 20:07:40 -0400

    Fixed missing comma in googleMaps, fixed Open Secrets from autocalling

--- assets/javascript/googleMaps.js
+++ assets/javascript/googleMaps.js
@@ -1,12 +1,13 @@
 var googleMapsAPI = 'AIzaSyAQ34rbfQcs_hp036e8ORnMuoAfULzj74U';
 var longitude, latitude;
+
 var fullState = ["alabama", "alaska", "arizona", "arkansas", "california", "colorado", 
 "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", 
 "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", 
 "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", 
 "nebraska", "nevada", "new hampshire",	"new jersey", "new mexico", "new york", 
 "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", 
-"rhode island", "south carolina", "south dakota", "tennessee", "texas"	"utah", 
+"rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", 
 "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming"];	
 
 var shortState = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", 
