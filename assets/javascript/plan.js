4996908e8c845e1bfcbdd05ee97ee90d5e55ce37
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-05-18 18:17:27 -0400

    Put input check in plan

--- assets/javascript/plan.js
+++ assets/javascript/plan.js
@@ -2,6 +2,17 @@
 User enters site
 	User enters information into search bar
 		CHECK INPUT:
+
+		if(isNaN(input) && input.length > 2) {
+			function stateValid(input);
+		}
+		else if(isNaN(input) && input.length === 2) {
+			function initalValid(input);
+		}
+		else {
+			function zipcodeIsValid(input);
+			function findStateAndCity(zipCode)
+		}
 			IF valid Zipcode:
 				FindCityandState(query);
 			IF State Name:
