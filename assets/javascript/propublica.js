c58226b960fa01ae85fae0f0c0a5d751872c512d
Brian K <karabinchak.brian@gmail.com>
2017-05-19 11:34:31 -0400

    Added social media symbols and committees to 4th panel

--- assets/javascript/propublica.js
+++ assets/javascript/propublica.js
@@ -1,6 +1,6 @@
 function appendRepMember (name, party, district, twitterHandle, crID) {
   $('#houseMembers').append(
-            '<li class=\"collection-item avatar rep\" crid = ' + crID + ' name='+name+'>' +
+            '<li class=\"collection-item avatar rep ' + party + '-bg \" crid = ' + crID + ' name='+name+'>' +
                   '<img src="https://twitter.com/' + twitterHandle +'/profile_image?size=original" alt="" class="circle">' +
                   '<span class="title">' + name + '</span>' +
                   '<p>Party: '+ party + ' <br> ' +
@@ -12,7 +12,7 @@ function appendRepMember (name, party, district, twitterHandle, crID) {
 
 function appendSenateMember (name, party, twitterHandle, crID) {
   $('#senateMembers').append(
-            '<li class=\"collection-item avatar sen\" crid = ' + crID + ' name='+name+'>' +
+            '<li class=\"collection-item avatar sen ' + party + '-bg \" crid = ' + crID + ' name='+name+'>' +
                   '<img src="https://twitter.com/' + twitterHandle +'/profile_image?size=original" alt="" class="circle">' +
                   '<span class="title">' + name + '</span>' +
                   '<p>Party: '+ party + ' <br> ' +
@@ -22,7 +22,30 @@ function appendSenateMember (name, party, twitterHandle, crID) {
             );
 }
 
-function clearMembers() {
+function populateFourthPanel(name, twitter, facebook, committees){
+  //remove previous info
+  $('#contactDiv').html('');
+  $('#contactDiv').removeClass('green');
+  $('#contactDiv').removeClass('white-text');
+  //Populate 4th
+  $('#contactDiv').append('<h4>' + name + '</h4>');
+  for (var i = 0; i<committees.length; i++) {
+    $('#contactDiv').append('<p>' + committees[i] + '</p>');
+  }
+  $('#contactDiv').append('<h4>Social Media</h4>');
+  //if twitter exists, then link it
+  if (twitter != '') {
+    //pic linked to twitter
+    $('#contactDiv').append('<a href="https://twitter.com/'+twitter+'"><img src="assets/images/twitter_bird.png" height="64px" width="80px"></a>');
+  }
+  //if facebook links, then link it
+  if (facebook != '') {
+    //pic linked to facebook
+    $('#contactDiv').append('<a href="https://www.facebook.com/'+facebook+'"><img src="assets/images/facebook_symbol.png" height="64px" width="64px"></a>');
+  }
+}
+
+function clearMembers () {
   $('#senateMembers').empty();
   $('#houseMembers').empty();
 }
@@ -88,6 +111,15 @@ $("#houseMembers").on("click", ".rep", function() {
           console.log('MEMBER RESULTS:');
           console.log('================');
           console.log(memberResults);
+          // populate 4th panel first since we already have info and Open Secets API slow
+          var currCommittees = [];
+          console.log('Committees');
+          for (var j = 0; j < memberResults[0].roles[0].committees.length; j++) {
+            currCommittees.push(memberResults[0].roles[0].committees[j].name);
+          }
+
+          populateFourthPanel(memberResults[0].first_name + ' ' + memberResults[0].last_name, memberResults[0].twitter_account, memberResults[0].facebook_account, currCommittees);
+
           drawChart(memberResults["0"].crp_id);
           Materialize.fadeInImage('#infoDiv');
 
@@ -113,9 +145,16 @@ $("#senateMembers").on("click", ".sen", function() {
        }).done(function(data) {
           var memberResults = data.results;
           console.log(memberResults);
-          // console.log(memberResults[0].roles[0].votes_with_party_pct);
-          // $("#infoPanel").html(memberResults[0].roles[0].votes_with_party_pct);
-          drawChart(memberResults["0"].crp_id);
+          // populate 4th panel first since we already have info and Open Secets API slow
+          var currCommittees = [];
+          console.log('Committees');
+          for (var j = 0; j < memberResults[0].roles[0].committees.length; j++) {
+            currCommittees.push(memberResults[0].roles[0].committees[j].name);
+          }
+
+          populateFourthPanel(memberResults[0].first_name + ' ' + memberResults[0].last_name, memberResults[0].twitter_account, memberResults[0].facebook_account, currCommittees);
+          //Now populate 3rd panel
+          drawChart(memberResults[0].crp_id);
           Materialize.fadeInImage('#infoDiv');
        });
 });
